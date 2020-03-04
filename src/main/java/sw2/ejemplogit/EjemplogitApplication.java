1b936423942b92b7531afa80fd4f29dec7cd4e94
Stuardo <stuardo.lucho@pucp.pe>
2020-03-04 12:34:45 -0500

    primer commit

---
+++ src/main/java/sw2/ejemplogit/EjemplogitApplication.java
@@ -0,0 +1,13 @@
+package sw2.ejemplogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemplogitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjemplogitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sw2/ejemplogit/EjemplogitApplicationTests.java
@@ -0,0 +1,13 @@
+package sw2.ejemplogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemplogitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
